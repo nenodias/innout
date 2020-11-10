bf87a787eae61a279deb85fd40a50cb0bd53b7d2
Nenodias <horacio.dias92@gmail.com>
2020-11-10 20:49:42 -0300

    Finishing lecture 243

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
 <h1>OK</h1>
+<?php 
+require_once(dirname(__FILE__, 2) . "/src/config/database.php");
+
+$sql = "SELECT * FROM users";
+$result = Database::getResultFromQuery($sql);
+while($row = $result->fetch_assoc()){
+    print_r($row);
+    echo "<br>";
+}
